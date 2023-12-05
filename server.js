0cbc566a69e5ece7d79163fe4a01d8260b689ccf
dostiny <dogyeom2@gmail.com>
2023-12-05 14:42:52 +0900

    mongoDB 연결 및 테스트

--- server.js
+++ server.js
@@ -3,14 +3,29 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
+const { MongoClient } = require("mongodb");
+
+let db;
+const url =
+  "mongodb+srv://admin:qwer1234@cluster0.tfgaly2.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("nodeStudy");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/news", (req, res) => {
-  res.send("오늘 비옴");
+  db.collection("post").insertOne({ title: "어쩌구" });
+  // res.send("오늘 비옴");
 });
