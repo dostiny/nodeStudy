1ab3b91870a34e47e4f47001ffc136961f2efa0e
dostiny <dogyeom2@gmail.com>
2023-12-05 13:40:23 +0900

    node 학습

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "../client/build/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/news", (req, res) => {
